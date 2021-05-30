f8d12d9f27465655c7f99b33693a43f096ba30e3
allips-cn <shiquanwang@live.cn>
2021-05-30 22:30:36 +0800

    vite 配置别名

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+//
+import path from 'path'
+const {resolve} = path
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve:{
+    alias:{
+      '@':resolve(__dirname,'src'),
+      'comps':resolve(__dirname,'src/components')
+    },
+  },
   plugins: [vue()]
 })
