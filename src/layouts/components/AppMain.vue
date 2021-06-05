2c8158a9e9a1c7aba8e22384a275f9fb8dae7e9c
wiser <shiquanwang@live.cn>
2021-06-05 23:46:58 +0800

    layout

--- src/layouts/components/AppMain.vue
+++ src/layouts/components/AppMain.vue
@@ -1,15 +1,13 @@
 <template>
-    <img alt="Vue logo" src="assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + Vite2" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite25" />
+    <comp></comp>
 </template>
 
 <script setup>
     // import HelloWorld from './components/HelloWorld.vue'
     import HelloWorld from 'comps/HelloWorld.vue'
+    import Comp from 'comps/Comp.vue'
     // This starter template is using Vue 3 experimental <script setup> SFCs
     // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
-
-<style lang="scss" scoped>
-
-</style>
