458f5d8162fdc035061516c6a9333303f0d2bc26
wiser <shiquanwang@live.cn>
2021-06-05 11:11:00 +0800

    样式管理 scss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// 路由
 import router from './router'
+// 状态
 
-createApp(App)
+// 全局样式
+import 'styles/index.scss'
+
+// UI
+import ElementPlus from 'plugins/element-plus'
+
+const app = createApp(App)
     .use(router)
+    .use(ElementPlus)
     .mount('#app')
